376f0b588b76381409df03feb0cd14298eb967b3
Saeid Ghafouri <acw498@frontend11.apocrita>
2021-11-22 23:26:32 +0000

    unecessary jobs deleted

--- data/jobs/jobs-dover.sh
+++
@@ -1,2 +0,0 @@
-ADDRESS=/homes/sg324/CCGrid-paper
-python ${ADDRESS}/experiments_scripts/learning_scripts/learners.py --mode real --local-mode true --config-folder constants-PPO-search --series 6 --type-env 3 --dataset-id 0 --workload-id 0 --use-callback true --checkpoint-freq 100
--- data/jobs/jobs.sh
+++
@@ -1,4 +0,0 @@
-ADDRESS=/homes/sg324/CCGrid-paper
-python ${ADDRESS}/experiments_scripts/learning_scripts/learners.py --mode real --local-mode true --config-folder constants-PPO --series 1 --type-env 3 --dataset-id 0 --workload-id 0 --use-callback true --checkpoint-freq 100
-python ${ADDRESS}/experiments_scripts/learning_scripts/learners.py --mode real --local-mode true --config-folder constants-PPO --series 2 --type-env 3 --dataset-id 1 --workload-id 0 --use-callback true --checkpoint-freq 100
-python ${ADDRESS}/experiments_scripts/learning_scripts/learners.py --mode real --local-mode true --config-folder constants-PPO --series 3 --type-env 3 --dataset-id 2 --workload-id 0 --use-callback true --checkpoint-freq 100
